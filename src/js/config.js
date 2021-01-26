af1b6be6dd3bb342d31172f8c9cbe057741be4d2
Mr-Vladislav <vladamrvic@gmail.com>
2021-01-26 19:39:38 +0100

    /

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes`;
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '1dbf8f21-a197-4b58-a0d7-fea1e24cc931';
+export const KEY = '196cac02-8f93-4c09-8642-c07546d1b90b';
 export const MODAL_CLOSE_SEC = 2.5;
